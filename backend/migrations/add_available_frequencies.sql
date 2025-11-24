411129f7edcffbacbccdec452a9105b3768ddbfc
Brad Brown Jr <bradbrownjr@gmail.com>
2025-11-23 22:13:44 -0500

    Remove applied SQL migration files

--- backend/migrations/add_available_frequencies.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add available_frequencies column to check_ins table
--- This column stores a JSON array of frequency IDs that a station can reach
--- Used for SKYWARN nets where operators indicate which frequencies they can monitor
-
-ALTER TABLE check_ins ADD COLUMN available_frequencies TEXT DEFAULT '[]';
--- backend/migrations/add_prefer_utc.sql
+++
@@ -1,2 +0,0 @@
--- Add prefer_utc column to users table
-ALTER TABLE users ADD COLUMN prefer_utc BOOLEAN DEFAULT 0;
