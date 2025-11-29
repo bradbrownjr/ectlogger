fa939f0456ab2e9fd9d07c9f5fc76ebcd0ce8360
Brad Brown Jr <bradbrownjr@gmail.com>
2025-11-28 23:47:40 -0500

    Change active speaker icon to speaking head emoji

--- frontend/src/pages/NetView.tsx
+++ frontend/src/pages/NetView.tsx
@@ -1237,7 +1237,7 @@ const NetView: React.FC = () => {
                             color={checkIn.id === activeSpeakerId ? 'primary' : 'default'}
                             title="Mark as active speaker"
                           >
-                            🎤
+                            🗣️
                           </IconButton>
                         )}
                         <IconButton
